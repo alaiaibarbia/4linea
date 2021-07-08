64efac2608ab1e76c66183c2cb20649124fc640c
Alaia Ibarbia <alaiaibarbia1@gmail.com>
2021-07-07 23:18:47 -0300

    arreglo muuuuuchos errores

--- prototipo.py
+++ prototipo.py
@@ -17,54 +17,68 @@ def contenidoColumna(nro_columna, tablero):
 
 def contenidoFila(nro_fila, tablero):
 	fila = []
-	for columna in tablero:_ 
-		celda = columna[nro_fila - 1]
+	for celda in tablero[nro_fila-1]:
 		fila.append(celda)
 	return fila
 
-def completarTableroEnOrden (secuencia, tablero):
-	for indice, columna in enumerate(secuencia):
-		fichaNumero = 1 + (indice % 2)
-		soltarFichaEnColumna(fichaNumero, columna, tablero)
-	return tablero
- 
 def soltarFichaEnColumna(ficha, columna, tablero):
 	for fila in range (6, 0, -1):
 		if tablero [fila - 1] [columna - 1] == 0:
 		   tablero [fila - 1] [columna - 1] = ficha
 		   return
 
+def completarTableroEnOrden(secuencia, tablero):
+	ficha = 1
+	for columna in secuencia: 
+		soltarFichaEnColumna(ficha,columna,tablero)
+		if ficha==1:
+			ficha=2
+		else:
+			ficha=1
+	return tablero 
+ 
 def dibujarTablero(tablero):
-	print('')
-	for fila in tablero:
-		for celda in fila:
-			if celda == 0:
-			   print('0', end= '')
-			else:
-				if celda == 1:
-			   print(' %s ' % celda, end= '')
-			else:
-				print(' %s ' %celda, end='')
-		printf('')
-	   print('')
+	for fila in range(0,6):
+	print("|",end="")
+	for columna in range(0,7):
+		if tablero[fila][columna]==0:
+			print(" ", end="")
+	else:
+		 print(tablero[fila][columna],end="")
+    print("|",end="")
+    print();
+  print("+-------+",end="")
+  print();
+  return 
 
-def contenidoFila(nroFila, tablero):
-	return tablero[fila - 1]
+def todasfilas(tablero):
+	return tablero 
 
 def secuenciaValida(secuencia):
 	for columna in secuencia:
-		if columna < 1 or columna > 7:
+		if (columna < 1 or columna > 7):
 		return False
 	return True
+
+def todascolumnas(tablero):
+    for columna in range(0,6,1):
+        for fila in range(0,6,1):
+            celda=tablero[fila][columna]
+            if(celda):
+                print(celda,end='')
+            else:
+                print('0')
+        print(" \n ")
 
 secuencia = [1, 2, 3, 1]
 
 tablero = []
 if secuenciaValida(secuencia):
-	tablero = completarTableroEnOrden(secuencia)
+    tablero = completarTableroEnOrden(secuencia,tableroVacio())
+    dibujarTablero(tablero)
 else:
-	print("Las columnas deberian ir de 1 al 7")
+    print("Las columnas deberian ir de 1 al 7")
 
 print(contenidoColumna(2, tablero))
-
-return
+# todascolumnas(tableroVacio())
+# todasfilas(tableroVacio())
